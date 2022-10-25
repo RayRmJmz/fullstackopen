483f7d932ccb45f989efbece34222951394c4168
Raymundo <raayrmjmz@gmail.com>
2022-10-25 11:14:05 -0500

    1.13*: anecdotes step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
-const Next = (props) => <div><button onClick={props.handleClick}>{props.text}</button></div>
+const MyBtn = (props) => <button onClick={props.handleClick}>{props.text}</button>
+const Mytext = (props) => <div> {props.text}</div>
 
 const App = () => {
   const anecdotes = [
@@ -14,14 +15,26 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
 
-  console.log(anecdotes.length)
-  const setToValue = (max)  => setSelected(Math.floor(Math.random() * (max-1)+1))
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * (max))
+  } 
+
+  const setToValue = (max)  => setSelected(getRandomInt(max))
+  
+  const setToVote = () =>{
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
-      <Next handleClick={() => setToValue(anecdotes.length)} text='next anecdote'/>
+      <Mytext  text={anecdotes[selected]} />
+      <Mytext  text={`has ${points[selected]} votes`} />
+      <MyBtn handleClick={() => setToVote()} text='vote'/>
+      <MyBtn handleClick={() => setToValue(anecdotes.length)} text='next anecdote'/>
     </div>
   )
 }
