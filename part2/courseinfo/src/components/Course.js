2cc88495060dade7ac9034944486ed0db6416b4f
Raymundo <raayrmjmz@gmail.com>
2022-10-25 18:50:48 -0500

    2.4: Course information step9

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,12 +1,23 @@
-const Header = ({ tittle }) => <h1>{tittle}</h1>
+const Header = ({ tittle }) => <div><h1>{tittle}</h1></div>
 
-const Part = ({ part }) => {
+const Part = ({ course }) => {
   return (
-    <>
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    </>
+    <div>
+      <p>{course.name}</p>
+    </div>
+  
+  )
+}
+
+const CoursePart = ({ course }) => {
+  return (
+    <div>
+      <h3>{course.name}</h3>
+      {course.parts.map(courses => 
+          <Part key={courses.id} course={courses} />
+      )}
+      <Total part={course.parts} />
+    </div>
   )
 }
 
@@ -14,20 +25,20 @@ const Total = ({ part }) => {
   const total = part.reduce((s, p) => s + p.exercises, 0)
 
   return (
-    <>
       <b>total of {total} exercises</b>
-    </>
   )
 }
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
+  
   return (
     <div>
-      <Header tittle={course.name} />
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-      <Total part={course.parts} />
+      <Header  tittle='Web development curriculum' />
+      {
+        courses.map(course => 
+          <CoursePart key={course.id} course={course} />
+        )
+      }
     </div>
   )
 }
