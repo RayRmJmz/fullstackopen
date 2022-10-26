21996fb3b03ad806850e2c53cd2f4df6d11adc1d
Raymundo <raayrmjmz@gmail.com>
2022-10-25 22:50:14 -0500

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,44 @@
 import { useState } from 'react'
 
-const Numbers = ({numbers}) => {
+const Filter = ({ filterName, setFilterName }) => {
+  const handleFilterChange = (event) => setFilterName(event.target.value)
+  
   return (
-    <div>{numbers.name} {numbers.number}</div>
+    <p>
+      filter shown with
+      <input value={filterName} onChange={handleFilterChange} />
+    </p>
   )
 }
 
+const Person = ({ name, number,  }) => {
+  return (
+    <p>
+      {name} {number}
+    </p >
+  )
+}
+const Persons = ({ persons }) => (
+  <>
+    {persons.map(p =>
+      <Person
+        key={p.id}
+        name={p.name}
+        number={p.number}
+      />
+    )}
+  </>
+)
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '0123456789' 
-    }
-  ]) 
-
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  
+  const [filterName, setFilterName] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -34,6 +60,9 @@ const App = () => {
     
   }
 
+  const filteredPersons = persons.filter( (x) => 
+    x.name.toLowerCase().includes(filterName.toLowerCase()))
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -45,6 +74,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Filter
+        filterName={filterName}
+        setFilterName={setFilterName}
+      />
+
+      <h2>Add a new</h2>
+
       <form onSubmit={addPerson}>
         <div>
           name: 
@@ -64,13 +101,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      {
-        persons.map(person => 
-          <Numbers key={person.name} numbers={person} />
-        )
-      }
-     
+      <Persons persons={filteredPersons} />
     </div>
   )
 }
